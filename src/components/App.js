162b2db565b64f19fa28151a05a0acf2ed786057
ifme18 <mainakenken188@gmail.com>
2024-11-15 11:57:59 +0300

    kk

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import { user } from "../data/data"; // Assuming data is imported from data.js
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
